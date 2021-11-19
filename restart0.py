3e0df999223be4b66f1904c89d301c39f4954ca7
sodonova <54714602+sodonova@users.noreply.github.com>
2021-11-19 18:13:33 -0500

    Begin adding themes, struggle

--- restart0.py
+++ restart0.py
@@ -1,35 +1,96 @@
+import time
+
 import streamlit as st
+import os
 import json
 
 # st.title("Restart: Write our resume")
 MAX_LINE_LEN = 100
+CONFIG_FOLDER = ".streamlit"
+CONFIG_FILE = "config.toml"
+THEMES_FILE = "themes.json"
+
 @st.cache
 def load_df():
-    with open('resume.json') as jsonfile:
+    with open('resume.json', 'r') as jsonfile:
         loaded_obj = json.load(jsonfile)
     # loaded_obj['Sections'] = loaded_obj['Sections'][:-2]
-    return loaded_obj
+    with open(THEMES_FILE, 'r') as jsonthemes:
+        themes = json.load(jsonthemes)
+    return loaded_obj, themes
 def init_session_states(section):
     if section not in st.session_state:
-        print('not yet in ', section)
+        # print('not yet in ', section)
         st.session_state[section] = list(resume[section].keys())[0]
     else:
-        print('key',section,'in session as',st.session_state[section])
+        pass
+        # print('key',section,'in session as',st.session_state[section])
     if f'{section}_col' not in st.session_state:
-        print('col not yet in ', section)
+        # print('col not yet in ', section)
         st.session_state[f'{section}_col'] = 0
     else:
-        print('col key',section,'in session as',st.session_state[f'{section}_col'])
+        pass
+        # print('col key',section,'in session as',st.session_state[f'{section}_col'])
+def update_theme():
+    # chosen_theme = st.session_state['theme']
+    # with open(os.path.join(CONFIG_FOLDER, CONFIG_FILE), 'w') as config:
+    #     with st.spinner():
+    #         config.write(themes[chosen_theme])
+    # print('update theme ran', chosen_theme)
+    st.session_state['updated'] = True
+
+resume, themes = load_df() # resume as dict from json
 
 
-resume = load_df() # resume as dict from json
 # resume['Sections'] = resume['Sections'][:-2]
-
+st.markdown(f'# {resume["Name"]}')
 # print(resume['Education']['GPA'])
 st.sidebar.markdown("""## Welcome!  \nRestart the resume process. 
                     You should have a personalized version of my resume - after all, you know 
                     what's relevant best. Start typing in the fields below to generate a resume.""")
-st.markdown(f'# {resume["Name"]}')
+# Make theming possible. Because the onchanged function occurs after the initial page runs,
+# this ugly code has to re run it from the beginning.
+# The flow is:
+# selectbox is chosen from, chosen sets 'theme'->
+# if updated is false ->
+# start rerun -> update_theme sets updated true ->
+# continue rerun, if 'updated' sets 'updated' to false writes config, reruns ->
+# 'updated' is false 'theme' exists and is new_theme
+if 'updated' not in st.session_state:
+    print('STATE NOT RETAINED UPDATED')
+    st.session_state['updated'] = False
+if 'theme' not in st.session_state:
+    print('STATE NOT RETAINED THEME')
+    st.session_state['theme'] = "Professional"
+    print('update called for first time')
+    update_theme()
+theme_list = list(themes.keys())
+chosen_theme = st.sidebar.selectbox('Theme',
+                                    theme_list,
+                                    index=0,
+                                    on_change=update_theme)
+# print('chosen theme', type(chosen_theme))
+st.session_state['theme'] = chosen_theme
+print('after selectbox', chosen_theme)
+
+if st.session_state['updated'] == True:
+    st.session_state['updated'] = False
+    # new_theme = st.session_state['theme']
+    config = open(os.path.join(CONFIG_FOLDER, CONFIG_FILE), 'w')
+        # with st.spinner():
+    config.write(themes[chosen_theme])
+    config.close()
+    print('if updated wrote', chosen_theme, 'updated now false')
+
+    # st.stop()
+    time.sleep(3)
+    print('rerun')
+    st.experimental_rerun()
+
+# st.experimental_rerun()
+# have them interact with something to trigger a rerun
+# _ = st.sidebar.button('Set theme')
+
 # form = st.sidebar.form(key='arbitrary_string')
 selected_sections = st.sidebar.multiselect('Sections', resume['Sections'], resume['Sections'])
 for section in selected_sections:
